091dad042f6fa78c9e1e01d13ba9d7e08a72df32
Uilliam Santos <uilliam2011@gmail.com>
2024-07-06 23:41:29 -0300

    Update app.py

--- Python/Sabor_Express/app.py
+++ Python/Sabor_Express/app.py
@@ -1,11 +1,132 @@
-print('𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤')
+
+import os
+
+
+#criação de Lista
+#restaurantes = ['Pizza','Sushi']
+
+#dicionario
+restaurantes = [{'nome':'Pizaria ABC','categoria':'Italiana','ativo':False},
+                {'nome':'Sushi','categoria':'Japonesa','ativo':True},
+                {'nome':'Hamburgueria','categoria':'Pizza','ativo':True}
+]
+
+
+def exibir_nome_programa():
+    print('𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤')
+
+def exibir_opcoes():
 ## print 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair\n')
+    print('1. Cadastrar Restaurantes')
+    print('2. Listar Restaurantes')
+    print('3. Alter estado do Restaurantes')
+    print('4. Sair\n')
 
-## input 
+# criando função def 
 
-opção_escolha = input('Escolha uma opção: ')
-print(f'Voce escolheu a opção : {opção_escolha}' )
+def finalizar_app():
+    exibir_subtitulo('Finalizando App')
+
+def opcao_invalida():
+    print('Opção inválida!')
+    main()
+
+def voltar_menu():
+    input('\nDigite uma tecla para voltar o meu principal: ')
+    main()
+
+## como essa função de exibir subtitulo estava sendo usada em diversas partes do caodigo foi criada uma função
+def exibir_subtitulo(texto):
+     os.system('cls')
+     linha = '*'  * (len(texto) + 4)
+     print(linha)
+     print(texto)
+     print(linha)
+
+def cadastrar_novo_restaurente():
+    exibir_subtitulo('Cadastros de Novos restaurantes\n')
+
+    nome_restaurente = input('Digite o nome do restaurante que deseja cadastrar:\n')
+    categoria = input(f'Digite a categoria do restaurante {nome_restaurente} : ')
+
+    dados_do_restaurante = {'nome':nome_restaurente,'categoria':categoria,'ativo':False}
+    restaurantes.append(dados_do_restaurante)
+
+    print(f'Restaurente {nome_restaurente} foi cadastrado com sucesso! \n')
+    voltar_menu()
+
+def listando_restaurantes():
+    exibir_subtitulo('Listando Restaurantes:\n')
+    
+    print(f'{'Nome Restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | {'Estado'}')
+
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f'- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
+    
+    voltar_menu()
+
+def alternar_estado_restaurante():
+     exibir_subtitulo('Alternando o estado do restaurante:\n')
+
+     nome_restaurante = input('Digite o nome do restaurante que deseja alterar o estado:')
+     restaurante_encontrado = False
+     for restaurante in restaurantes:
+         if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mesagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+            print(mesagem)
+     if not restaurante_encontrado:
+        print('o restaurante não foi encontrado')
+     voltar_menu()
+
+def escolher_opcao():
+# try -except para não "quebrar o code quando digita algo fora do padrão"
+    try:
+        ## input 
+        opção_escolha = int(input('Escolha uma opção: '))
+
+## Match é uma alternativa pro if esle elif
+
+        match opção_escolha:
+            case 1:
+                cadastrar_novo_restaurente()
+            case 2:
+                listando_restaurantes()
+            case 3:
+                alternar_estado_restaurante()
+            case 4:
+                print('Finalizar app')
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
+
+
+# ## if else
+
+#     if opção_escolha == 1:
+#         print('1. Cadastrar Restaurantes')
+#     elif opção_escolha == 2:
+#         print('2. Listar Restaurantes')
+#     elif opção_escolha == 3:
+#         print('3. Ativar Restaurantes')
+#     else:
+#         finalizar_app()
+
+
+## deixando o programa como Main
+
+def main():
+    os.system('cls')
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+if __name__ == '__main__':
+    main()
