ebad5ab89cc0c6b815e928cfada398365ae90888
Uilliam Santos <uilliam2011@gmail.com>
2024-07-06 21:08:47 -0300

    Licao_1

---
+++ Python/Sabor_Express/app.py
@@ -0,0 +1,11 @@
+print('𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤')
+## print 
+print('1. Cadastrar Restaurante')
+print('2. Listar Restaurante')
+print('3. Ativar Restaurante')
+print('4. Sair\n')
+
+## input 
+
+opção_escolha = input('Escolha uma opção: ')
+print(f'Voce escolheu a opção : {opção_escolha}' )
